348c1af9dc14387037e32e0d4982d5bb4c6b258f
gabrielpandrade <pintoandradegabriel@gmail.com>
2022-02-22 14:28:16 -0300

    Metodos

---
+++ src/br/dio/oo/metodos/Carro.java
@@ -0,0 +1,49 @@
+package br.dio.oo.metodos;
+
+public class Carro {
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    //Construtor
+
+    public Carro() {
+
+    }
+
+    public Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    //Setters e Getters
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    double totalValorTanque(double valorCombustivel){
+        return getCapacidadeTanque()*valorCombustivel;
+    }
+}
